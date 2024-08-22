0e40bf4d3967c7356b8f5555006a9e21d00aea35
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-22 11:48:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// Complete the js code
-function Car(make, model) {}
+// Car constructor function
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method added to the Car prototype
+Car.prototype.getMakeModel = function() {
+    return this.make + ' ' + this.model;
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// SportsCar constructor function
+function SportsCar(make, model, topSpeed) {
+    // Inherit properties from Car
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method added to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed + ' km/h';
+};
+
+// Example usage
+const myCar = new Car('Toyota', 'Corolla');
+console.log(myCar.getMakeModel()); // Outputs: "Toyota Corolla"
+
+const mySportsCar = new SportsCar('Ferrari', '488', 330);
+console.log(mySportsCar.getMakeModel()); // Outputs: "Ferrari 488"
+console.log(mySportsCar.getTopSpeed()); // Outputs: "330 km/h"
